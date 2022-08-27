32dbb8f60849e3a47a93a8f506789aa1a1857e1d
augustal <Alan.Augustine@cba.com>
2022-08-27 17:32:40 +1000

    updates

--- scripts/codedeploy_scripts/codedeploy_before_install.sh
+++ scripts/codedeploy_scripts/codedeploy_before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "---------- Start codedeploy_before_install.sh"
+rm -Rf /var/myapp
 echo "---------- End codedeploy_before_install.sh"
--- scripts/codedeploy_scripts/codedeploy_stop_server.sh
+++ scripts/codedeploy_scripts/codedeploy_stop_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "------------- Start codedeploy_stop_server.sh"
+# Kill Flask Server process
+sudo pkill python3
 echo "------------- End codedeploy_stop_server.sh"
