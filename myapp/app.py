b31c93d496402b4fa2fabd9d1f42e4679ebf16f9
augustal <Alan.Augustine@cba.com>
2022-08-27 17:38:24 +1000

    port update

--- myapp/app.py
+++ myapp/app.py
@@ -9,7 +9,7 @@ def index():
     return render_template('base.html')
 
 def main():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
 
 if __name__ == '__main__':
     main()
