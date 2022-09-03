d03d065e9c818bacf7dd33793ef47c4dda40bea8
augustal <Alan.Augustine@cba.com>
2022-09-03 13:07:37 +1000

    Add EC2 profile and mac.sh

---
+++ mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+open -a "Google Chrome" 'https://us-east-1.console.aws.amazon.com/' \
+                        'https://github.com/alan-augustine/aws_codepipeline_python_demo' \
+                        'https://github.com/alan-augustine/terraform-iacdevops-with-aws-codepipeline/blob/master/buildspec-dev.yml' \
+                        'https://github.com/alan-augustine/my_knowledge_base/blob/master/Terraform/Terraform_notes.txt'
--- terraform-manifests/ec2-public.tf
+++ terraform-manifests/ec2-public.tf
@@ -6,5 +6,8 @@ module "ec2-instance" {
   ami = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0fdb73b8924436949"]
+  # Ref: https://github.com/terraform-aws-modules/terraform-aws-ec2-instance/blob/dbf16a26e3077ac1b9ebc7f674f81bd710c438fa/variables.tf#L91
+  # not available in complete exaple
+  iam_instance_profile = "CodeDeploy-S3-Access"
   user_data = filebase64("${path.module}/ec2-user-data.sh")
 }
